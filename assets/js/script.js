163bc00430e1a89571d4dcf520d013b067de1088
caketaster <>
2023-05-04 16:15:45 +0000

    Add code to increment correct and incorrect score tallies

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,10 @@ document.addEventListener("DOMContentLoaded", function () {
                 runGame(gameType);
             }
         });
-
-        runGame(addition);
     }
+
+    runGame("addition");
+
 });
 
 /**
@@ -47,8 +48,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right :D");
+        incrementScore();
     } else {
         alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}.`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -71,13 +74,21 @@ function calculateCorrectAnswer() {
     }
 
 }
-
+/**
+ * Gets the current score from the DOM and increments it by one
+ */
 function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by one
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
